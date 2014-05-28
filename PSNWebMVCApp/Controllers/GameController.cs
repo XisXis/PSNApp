6fb990c5af9c8268f5e33669dfe18a5957d6239e
joao.mira@gmail.com <mira12xis>
2014-05-29 00:59:47 +0100

    Database update

---
+++ PSNWebMVCApp/Controllers/GameController.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PSNWebMVCApp.Models;
+using System.Xml;
+
+namespace PSNWebMVCApp.Controllers
+{
+    public class GameController : Controller
+    {
+        private GameContext db = new GameContext();
+
+        //
+        // GET: /Game/
+
+        public ActionResult Index()
+        {
+            return View(db.Games.ToList());
+        }
+
+        //
+        // GET: /Game/Details/5
+
+        public ActionResult Details(int id = 0)
+        {
+            Game game = db.Games.Find(id);
+            if (game == null)
+            {
+                return HttpNotFound();
+            }
+            return View(game);
+        }
+
+        //
+        // GET: /Game/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        public ActionResult UpdateGames()
+        {
+            Chilkat.Http http = new Chilkat.Http();
+
+            bool success;
+            success = http.UnlockComponent("30-day trial");
+            if (success != true)
+            {
+                return View();
+            }
+
+            string html;
+            //html = http.QuickGetStr("http://www.exophase.com/platform/ps3-games/");
+            html = http.QuickGetStr("http://psntrophyleaders.com/games/1?sort=title-all-asc");
+            if (html == null)
+            {
+                //TextBox1.Text += http.LastErrorText + "\r\n";
+                return View();
+            }
+
+            Chilkat.HtmlToXml htmlToXml = new Chilkat.HtmlToXml();
+
+            success = htmlToXml.UnlockComponent("30-day trial");
+            if (success != true)
+            {
+                return View();
+            }
+
+            htmlToXml.XmlCharset = "utf-8";
+            htmlToXml.Html = html;
+
+            string xml;
+            xml = htmlToXml.ToXml();
+
+            success = htmlToXml.WriteStringToFile(xml, "C:/temp/out.xml", "utf-8");
+
+            XmlDocument doc = new XmlDocument();
+            doc.LoadXml(xml);
+            XmlNodeList gamesTitleList = doc.SelectNodes("//*[@class='gameTitle']");
+
+            return View();
+        }
+
+        //
+        // POST: /Game/Create
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Game game)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Games.Add(game);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(game);
+        }
+
+        //
+        // GET: /Game/Edit/5
+
+        public ActionResult Edit(int id = 0)
+        {
+            Game game = db.Games.Find(id);
+            if (game == null)
+            {
+                return HttpNotFound();
+            }
+            return View(game);
+        }
+
+        //
+        // POST: /Game/Edit/5
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Game game)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(game).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(game);
+        }
+
+        //
+        // GET: /Game/Delete/5
+
+        public ActionResult Delete(int id = 0)
+        {
+            Game game = db.Games.Find(id);
+            if (game == null)
+            {
+                return HttpNotFound();
+            }
+            return View(game);
+        }
+
+        //
+        // POST: /Game/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Game game = db.Games.Find(id);
+            db.Games.Remove(game);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ PSNWebMVCApp/Models/PSNDataModel.edmx.sql
@@ -0,0 +1,63 @@
+
+-- --------------------------------------------------
+-- Entity Designer DDL Script for SQL Server 2005, 2008, 2012 and Azure
+-- --------------------------------------------------
+-- Date Created: 05/29/2014 00:15:33
+-- Generated from EDMX file: c:\users\xis\documents\visual studio 2013\Projects\PSNWebMVCApp\PSNWebMVCApp\Models\PSNDataModel.edmx
+-- --------------------------------------------------
+
+SET QUOTED_IDENTIFIER OFF;
+GO
+USE [PSNDatabase];
+GO
+IF SCHEMA_ID(N'dbo') IS NULL EXECUTE(N'CREATE SCHEMA [dbo]');
+GO
+
+-- --------------------------------------------------
+-- Dropping existing FOREIGN KEY constraints
+-- --------------------------------------------------
+
+
+-- --------------------------------------------------
+-- Dropping existing tables
+-- --------------------------------------------------
+
+IF OBJECT_ID(N'[dbo].[GameSet]', 'U') IS NOT NULL
+    DROP TABLE [dbo].[GameSet];
+GO
+
+-- --------------------------------------------------
+-- Creating all tables
+-- --------------------------------------------------
+
+-- Creating table 'GameSet'
+CREATE TABLE [dbo].[GameSet] (
+    [Id] int IDENTITY(1,1) NOT NULL,
+    [Title] nvarchar(max)  NOT NULL,
+    [Platform] nvarchar(max)  NOT NULL,
+    [ImageString] nvarchar(max)  NOT NULL,
+    [Difficulty] nvarchar(max)  NOT NULL,
+    [DifficultyPoints] nvarchar(max)  NOT NULL,
+    [Completed] nvarchar(max)  NOT NULL,
+    [Owned] nvarchar(max)  NOT NULL,
+    [RarityPoints] nvarchar(max)  NOT NULL
+);
+GO
+
+-- --------------------------------------------------
+-- Creating all PRIMARY KEY constraints
+-- --------------------------------------------------
+
+-- Creating primary key on [Id] in table 'GameSet'
+ALTER TABLE [dbo].[GameSet]
+ADD CONSTRAINT [PK_GameSet]
+    PRIMARY KEY CLUSTERED ([Id] ASC);
+GO
+
+-- --------------------------------------------------
+-- Creating all FOREIGN KEY constraints
+-- --------------------------------------------------
+
+-- --------------------------------------------------
+-- Script has ended
+-- --------------------------------------------------
